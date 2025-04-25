7d036dff4a427a31e92f53ea10783ef17f9ef788
Joe Kollie <joekollie020@gmail.com>
2025-04-24 22:59:05 -0500

    Added code to index.js file to enhance logic

--- index.js
+++ index.js
@@ -3,7 +3,30 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi there!')
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <input name="passwordConfirmation" placeholder="password confirmation" />
+                <button>Sign Up</button>           
+            </form>
+        
+        </div>
+        `)
+})
+
+app.post('/', (req, res) => {
+    req.on('data', data => {
+        const parsed = data.toString('utf8').split('&');
+        const formData = {};
+        for (let pair of parsed) {
+            const [key, value] = pair.split('=');
+            formData[key] = value;
+        }
+        console.log(formData);
+    });
+    res.send('Account created!!!');
 })
 
 
