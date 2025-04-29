0979fce94105de8a50a60d9a2be722a62de66689
Joe Kollie <joekollie020@gmail.com>
2025-04-28 23:51:04 -0500

    Added code to create and save records in json format

--- repos/users.js
+++ repos/users.js
@@ -16,22 +16,32 @@ class UsersRepository {
         }
       
     }
-     async getAll() {
-        //Open the file called this.filename
-        const contents = await fs.promises.readFile(this.filename, { 
-            encoding: 'utf-8'});
-    
-        //parse the contents
-        const data = JSON.parse(contents);
 
-        //Return the parsed data
-        return data;
+    async getAll() {
+        return JSON.parse(
+            await fs.promises.readFile(this.filename, {
+                encoding: 'utf-8'
+            })
+        );
+    }
 
-     }
+    async create(attrs) {
+        const records = await this.getAll();
+        records.push(attrs)
+        
+        await this.writeAll(records);
+    }
+
+    async writeAll(records) {
+        await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+    }
+     
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
+
+     await repo.create({email: 'test@test.com', password:'password'})
 
     const users = await repo.getAll();
 
